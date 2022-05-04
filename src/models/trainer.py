f3ad2c5714035087d25a64399d51b2ca320ea091
Shreshth Malik <shreshth_malik@yahoo.co.uk>
2022-05-04 14:57:06 +0100

    start building repo

---
+++ src/models/trainer.py
@@ -0,0 +1,19 @@
+"""trainer.py
+Utility functions and classes for model training.
+"""
+
+
+class Trainer():
+
+    def __init__(self, net, optimizer, criterion, device, scheduler=None):
+        """
+        Params:
+        - 
+        """
+
+        pass
+
+    def train(self, train_loader, epoch):
+        """
+        """
+        pass
